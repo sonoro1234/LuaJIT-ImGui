a490dc30aa88172ab11c639084ef875334302f10
Victor Bombi <sonoroster@gmail.com>
2020-08-31 18:36:20 +0200

    generation

--- lua/imgui/cdefs.lua
+++ lua/imgui/cdefs.lua
@@ -3334,10 +3334,6 @@ typedef struct{
  union {
   float f[16];
         Vec4 v[4];
-        struct { float m00, m01, m02, m03,
-                        m10, m11, m12, m13,
-                        m20, m21, m22, m23,
-                        m30, m31, m32, m33; };
     };
 }Mat4;
 typedef struct{
